b9fa8038537b8d80d2b82561e11c90adbf9178c3
Chris <christiaea@uia.no>
2023-04-21 10:19:55 +0200

    dekrypt

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Lordkissa97sem03simpleclient
+
+go 1.20
+
+require github.com/Lordkissa97/is105sem03 v0.0.0-20230421072257-35a623725a94
--- tcp_client.go
+++ tcp_client.go
@@ -27,7 +27,7 @@ func main() {
 	response := string(buf[:n])
 	log.Printf("reply from proxy: %s", response)
 
-	kryptertMelding := mycrypt.Krypter([]rune(os.Args[1]), mycrypt.ALF_SEM03, 4)
+	kryptertMelding := mycrypt.Krypter([]rune(os.Args[1]), mycrypt.AlfSem03, 4)
 	log.Println("Kryptert melding: ", string(kryptertMelding))
 	_, err = conn.Write([]byte(string(kryptertMelding)))
 }
